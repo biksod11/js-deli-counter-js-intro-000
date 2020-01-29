3a7320637db34a28e0204ccde1d827c56095ffb1
Bikram Sodhi <biksodhi@gmail.com>
2020-01-29 21:34:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function currentLine(line){
 function takeANumber(line, name){
   line.push(name)
 
-  return `Welcome ${name}. You are number ${line.length} in line.`
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 };
